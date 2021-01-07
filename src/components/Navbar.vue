ab85a18d856635ed4ad701a8a0c7d4eb25a5498d
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-01-07 23:03:37 +0700

    begin

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,52 @@
 <template>
-    <div>
-        <b-navbar toggleable="lg" type="dark" variant="dark">
-            <b-navbar-brand href="#" class="mx-auto">Indoxxi-ranked</b-navbar-brand>
-        </b-navbar>
-    </div>
+  <div>
+    <b-navbar>
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img
+            src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+            alt="Lightweight UI components for Vue.js based on Bulma"
+          />
+        </b-navbar-item>
+      </template>
+      <template slot="start">
+        <b-navbar-item href="#">
+          Home
+        </b-navbar-item>
+        <b-navbar-item href="#">
+          Documentation
+        </b-navbar-item>
+        <b-navbar-dropdown label="Info">
+          <b-navbar-item href="#">
+            About
+          </b-navbar-item>
+          <b-navbar-item href="#">
+            Contact
+          </b-navbar-item>
+        </b-navbar-dropdown>
+      </template>
+
+      <template slot="end">
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">
+              Log in
+            </a>
+          </div>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Navbar',
-    components: {}
-
-}
+  name: "Navbar",
+  components: {},
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-import {
-  BootstrapVue,
-  IconsPlugin
-} from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
-// Install BootstrapVue
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
-// instal fontawesome icon
-
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
